2ce84b8fbafa98ae2f08c163869faadd674cf6b3
Felicia Wold <felicia@ringseven.com>
2017-01-27 13:21:04 -0700

    added concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
@@ -6,9 +7,15 @@ class Portfolio < ApplicationRecord
   end
 
   after_initialize :set_defaults
+# originally had this. created a concern helper Placeholder
+  # def set_defaults
+  #   self.main_image ||= "http://placehold.it/600x400" 
+  #   self.thumb_image ||= "http://placehold.it/350x200" 
+  # end
+  # new code becomes
 
-  def set_defaults
-    self.main_image ||= "http://placehold.it/600x400" 
-    self.thumb_image ||= "http://placehold.it/350x200" 
+   def set_defaults
+    self.main_image ||= Placeholder.image_generator(height: '600', width:'400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width:'200') 
   end
 end
