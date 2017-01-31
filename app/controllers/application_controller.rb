cd647a1dde7ef2708829dbf983fe79a4b53bbf9d
Felicia Wold <felicia@ringseven.com>
2017-01-31 12:34:38 -0700

    minicommit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
- include SetSource
+  include SetSource
+
+  def current_user
+  super || OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email:"guest@guest.com")
+  end
+
 end
