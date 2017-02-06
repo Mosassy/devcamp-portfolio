be08a40bf957d7c4362f164bd3185a79a8193dca
Felicia Wold <felicia@ringseven.com>
2017-02-06 16:08:43 -0700

    integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    @copyright = MosassyViewTool::Renderer.copyright 'Felicia Wold', 'All rights reserved'
+  end
 end
